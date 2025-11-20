4426dbee2c42b1f8e559e42d5c407dcd96ff1410
yoshimaru <yoshi0410777@gmail>
2025-11-20 23:52:12 +0900

    繰り返し処理応用

---
+++ JavaScript_practice/2.js
@@ -0,0 +1,19 @@
+let number = 0;
+
+number += 1;
+console.log(number);
+
+number += 1;
+console.log(number);
+
+number += 1;
+console.log(number);
+
+number += 1;
+console.log(number);
+
+number += 1;
+console.log(number);
+
+
+
